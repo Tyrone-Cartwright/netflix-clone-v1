bb61b1538f50146c43a41923283b413a746f2c06
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2021-06-01 03:46:49 -0400

    added minor fix to Row.js and added some style to Row.css to resize imgs

--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
+import './Row.css';
 
-const Row = ({ title, fetchUrl }) => {
+const base_url = 'https://image.tmdb.org/t/p/original/';
+
+function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
 
   // A snippet of code which runs based on a specific condition/variable
@@ -22,17 +25,21 @@ const Row = ({ title, fetchUrl }) => {
     <div className='row'>
       <h2>{title}</h2>
 
-      <div className='row__poster'>
+      <div className='row__posters'>
         {/* several row__poster(s) */}
 
         {movies.map((movie) => (
-          <img src={movie.poster_path} alt={movie.name} />
+          <img
+            className='row__poster'
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
         ))}
       </div>
 
       {/* container -> posters */}
     </div>
   );
-};
+}
 
 export default Row;
